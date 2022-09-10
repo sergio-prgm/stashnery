f999f8dc0abea7a7bbd3736243cff7860c3e4384
sergio-dev <sergio-dev@outlook.com>
2022-09-10 12:52:24 +0200

    Feat: fetch products from API

---
+++ components/Card.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+
+
+interface Product {
+  name: string
+  price: number
+  description: string
+  image: string
+  availability: {
+    state: 'in-stock' | 'pending' | 'out-of-stock',
+    'items-left'?: number 
+  }
+}
+
+const props = defineProps({
+  product: Object as PropType<Product>
+})
+
+const availStyle = {
+  'in-stock': 'text-green-500',
+  'out-of-stock': 'text-red-600',
+  'pending': 'text-amber-600',
+}
+
+
+
+</script>
+
+<template>
+  <article class="bg-stone-300 rounded p-3 w-2/5">
+    <h3 class="font-bold text-xl">{{ product?.name }}</h3>
+    <p>{{ product?.description}}</p>
+    <small
+      class="font-medium"
+      :class="product && availStyle[product?.availability.state]"
+    >
+      {{product?.availability.state}}
+    </small>
+  </article>
+</template>
--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,23 @@
-export default defineEventHandler((event) => {
-  return {
-    api: 'items ready to launch!!'
+import { readFileSync} from 'fs'
+
+interface Product {
+  name: string
+  price: number
+  description: string
+  image: string
+  availability: {
+    state: 'in-stock' | 'pending' | 'out-of-stock',
+    'items-left'?: number 
   }
+}
+
+export default defineEventHandler((event) => {
+  const products = readFileSync('./public/products.json', 'utf8')
+
+  const data: Product[] = JSON.parse(products)
+  console.log('vvvv DATA vvv')
+
+  console.log('---------------------------------------')
+
+  return data
 })
