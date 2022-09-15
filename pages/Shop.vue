47181610f2d8b22017628a2f0af5661f48aa2a91
sergio-dev <sergio-dev@outlook.com>
2022-09-15 19:54:52 +0200

    Remove test cart

--- pages/Shop.vue
+++ pages/Shop.vue
@@ -2,7 +2,7 @@
 definePageMeta({
   layout: 'shop'
 })
-
+// TODO transition individual items with <transition
 // transform ~= map 
 // pick ~= select (only for single objects)
 const d = await useFetch('/api/product', { 
@@ -36,8 +36,5 @@ const filteredProducts = computed(() => {
   <div class="flex flex-row flex-wrap gap-2 px-4">
     <card v-for="product in filteredProducts" :key="product.name" :product="product" />
   </div>
-  <client-only >
-    <Cart />
-  </client-only>
 </div>
 </template>
