b84ff514af9b5c14de9f84b0e45143cb7dd6d6df
sergio-dev <sergio-dev@outlook.com>
2022-09-19 18:29:57 +0200

    Page: new shipping page

---
+++ pages/Shipping.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'default'
+})
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">Shipping and Returns</h1>
+  <section class="mt-4 max-w-prose leading-relaxed font-[Muli]">
+    <h2 class="mb-2 text-2xl font-semibold font-[BodoniModa]">Shipping</h2>
+
+    <p class="mb-2">We can ship to virtually any address in the world. Note that there are restrictions on some products, and some products cannot be shipped to international destinations.</p>
+    <p class="mb-2">When you place an order, we will estimate shipping and delivery dates for you based on the availability of your items and the shipping options you choose. Depending on the shipping provider you choose, shipping date estimates may appear on the shipping quotes page.</p>
+    <p class="mb-2">Please also note that the shipping rates for many items we sell are weight-based. The weight of any such item can be found on its detail page. To reflect the policies of the shipping companies we use, all weights will be rounded up to the next full pound.</p>
+
+  </section>
+
+  <section class="mt-4 max-w-prose leading-relaxed font-[Muli]">
+    <h2 class="mb-2 text-2xl font-semibold font-[BodoniModa]">Return Policy</h2>
+
+    <p class="mb-2">You may return most new, unopened items within 30 days of delivery for a full refund. We'll also pay the return shipping costs if the return is a result of our error (you received an incorrect or defective item, etc.).</p>
+    <p class="mb-2">You should expect to receive your refund within four weeks of giving your package to the return shipper, however, in many cases you will receive a refund more quickly. This time period includes the transit time for us to receive your return from the shipper (5 to 10 business days), the time it takes us to process your return once we receive it (3 to 5 business days), and the time it takes your bank to process our refund request (5 to 10 business days).</p>
+    <p class="mb-2">If you need to return an item, please Contact Us with your order number and details about the product you would like to return. We will respond quickly with instructions for how to return items from your order.</p>
+  </section>
+</template>
